5175610c791319c3fb0c57785f4b052ce18d5b9e
charlie.brown <cdb.binary@gmail.com>
2018-06-03 10:30:03 +0100

    updating addfishform

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,21 +20,4 @@ class Header extends React.Component {
 	}
 }
 
-// const Header = ({ tagline, age }) => (
-// 	<header className="top">
-// 		<h1>
-// 			Catch
-// 			<span className="ofThe">
-// 				<span className="of">of</span>
-// 				<span className="the">the</span>
-// 			</span>
-// 			Day
-// 		</h1>
-// 		<h3 className="tagline">
-// 			<span>
-// 				{tagline} {age}
-// 			</span>
-// 		</h3>
-// 	</header>
-// );
 export default Header;
--- src/components/StorePicker.jsx
+++ src/components/StorePicker.jsx
@@ -8,10 +8,10 @@ class StorePicker extends React.Component {
 		// 1. Stop form from submitting
 		event.preventDefault();
 		// 2. Get the text from that input
-		console.log(this.myInput);
-
+		const storeName = this.myInput.value.value
 		// 3. Change the page to /store/whatever-they-entered
-	}
+		this.props.history.push(`/store/${storeName}`);
+	};
 	render() {
 		return (
 			<form className="store-selector" onSubmit={this.goToStore}>
